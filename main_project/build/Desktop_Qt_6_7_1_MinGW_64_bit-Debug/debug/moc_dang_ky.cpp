c0352c2439d04605bddbaaf23ec76fadbe714218
langkhach612 <khoi0089567@huce.edu.vn>
2024-06-30 16:27:20 +0700

    update_2

--- main_project/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/debug/moc_dang_ky.cpp
+++ main_project/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/debug/moc_dang_ky.cpp
@@ -35,7 +35,13 @@ namespace {
 #ifdef QT_MOC_HAS_STRINGDATA
 struct qt_meta_stringdata_CLASSDang_kyENDCLASS_t {};
 constexpr auto qt_meta_stringdata_CLASSDang_kyENDCLASS = QtMocHelpers::stringData(
-    "Dang_ky"
+    "Dang_ky",
+    "on_btnHello_clicked",
+    "",
+    "on_btnInfor_clicked",
+    "on_btnQuestion_clicked",
+    "on_btnDangky_clicked",
+    "on_btnCapnhat_clicked"
 );
 #else  // !QT_MOC_HAS_STRINGDATA
 #error "qtmochelpers.h not found or too old."
@@ -48,12 +54,26 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSDang_kyENDCLASS[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   44,    2, 0x08,    1 /* Private */,
+       3,    0,   45,    2, 0x08,    2 /* Private */,
+       4,    0,   46,    2, 0x08,    3 /* Private */,
+       5,    0,   47,    2, 0x08,    4 /* Private */,
+       6,    0,   48,    2, 0x08,    5 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -66,16 +86,35 @@ Q_CONSTINIT const QMetaObject Dang_ky::staticMetaObject = { {
     nullptr,
     qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSDang_kyENDCLASS_t,
         // Q_OBJECT / Q_GADGET
-        QtPrivate::TypeAndForceComplete<Dang_ky, std::true_type>
+        QtPrivate::TypeAndForceComplete<Dang_ky, std::true_type>,
+        // method 'on_btnHello_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnInfor_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnQuestion_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnDangky_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnCapnhat_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
 } };
 
 void Dang_ky::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Dang_ky *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_btnHello_clicked(); break;
+        case 1: _t->on_btnInfor_clicked(); break;
+        case 2: _t->on_btnQuestion_clicked(); break;
+        case 3: _t->on_btnDangky_clicked(); break;
+        case 4: _t->on_btnCapnhat_clicked(); break;
+        default: ;
+        }
+    }
     (void)_a;
 }
 
@@ -95,6 +134,17 @@ void *Dang_ky::qt_metacast(const char *_clname)
 int Dang_ky::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 5)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 5;
+    }
     return _id;
 }
 QT_WARNING_POP
